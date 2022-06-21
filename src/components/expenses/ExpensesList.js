abbb04711781811656923ad7815c85c98be08b15
edyedy <edydavidgasser@gmail.com>
2022-06-21 18:45:15 +0200

    before assignment 3 and expenses list work

---
+++ src/components/expenses/ExpensesList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list__fallback">Found no expenses.</h2>
+    }
+
+    return <ul className="expenses-list">
+        {props.items.map((data) => {
+            return <ExpenseItem key={data.id} title={data.title} amount={data.amount} date={data.date}/>
+        })}
+    </ul>
+}
+
+export default ExpensesList;
